6b6e82f1553b95cfb2cc51d1b83bb7cdb89b8162
Abhishek Ghosh <abhi.ghosh3108@gmail.com>
2023-02-05 22:14:46 +0530

    changes made

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 function Header(props){
+    
+    
     return(<>
-    <h4>No. of Items in Cart  --{props.cart.length}</h4><br/>
+    {
+    (props.user.length > 0)?<><h4>No. of Items in Cart  --{props.cart.length}</h4>
     <h4>No. of Items in orders--{props.order.length}</h4>
-    <h4>Usename--{props.user.length}</h4>
+    <h4>Username--{props.user.length}</h4>
+    </>
+    :null
+    
+    }<br/>
+   
     
     </>)
 }
