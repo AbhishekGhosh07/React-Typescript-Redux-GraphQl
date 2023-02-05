ea690835ad1ed4494f7c90a1ba442af8f0d72976
Abhishek Ghosh <abhi.ghosh3108@gmail.com>
2023-02-05 22:31:09 +0530

    changes made

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 function Header(props){
     
-    
+    console.log("Header",props);
+    const user1 = props.user[0].idPass;
+const username = user1.username;
+const password = user1.password;
     return(<>
-    {
-    (props.user.length > 0)?<><h4>No. of Items in Cart  --{props.cart.length}</h4>
-    <h4>No. of Items in orders--{props.order.length}</h4>
-    <h4>Username--{props.user.length}</h4>
-    </>
-    :null
     
-    }<br/>
-   
+    
+
+    {/* // (props.user.length > 0)?<><h4>No. of Items in Cart  --{props.cart.length}</h4>
+    // <h4>No. of Items in orders--{props.order.length}</h4>
+    // <h4>Username--{props.user.length}</h4>
+    // </>
+    // :null
+     */}
+    <h4>{username}</h4>
+     
     
     </>)
 }
