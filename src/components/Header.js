83569dfc92a1b2d62796b789f3dd22ffd8bc99a6
Abhishek Ghosh <abhi.ghosh3108@gmail.com>
2023-02-06 15:57:40 +0530

    changes made

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 function Header(props){
-    
+    let navigate = useNavigate();
     console.log("Header",props);
     console.log("User Header",props.user);
     
    //console.log("H",props.user[0].users.username);
     return(<>
-    <h4>No. of Items in Cart  --{props.cart.length}</h4>
+        <center>
+        <h4>No. of Items in Cart  --{props.cart.length}</h4>
      <h4>No. of Items in orders--{props.order.length}</h4>
+        {(props.user.length==0)? <Button variant="primary" onClick={()=>navigate("/login")}>Login</Button>:null}
+    
+
+        </center>
+
+    
     
      
 
